1439e10b58bde931bd59932b6434df1ad545e52d
Jorge Gabitto <jgabitto1792@gmail.com>
2021-03-05 09:52:38 -0600

    added params to route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get 'auth/github/callback', to: 'omniauth#github'
+      get 'auth/github/callback:code', to: 'omniauth#github'
     end
   end
 end

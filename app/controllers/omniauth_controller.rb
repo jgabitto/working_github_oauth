d3ca4bab5ec9e9c0271338782f4b92bf430a2d10
Jorge Gabitto <jgabitto1792@gmail.com>
2021-03-05 15:57:50 -0600

    changed redirect_uri

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,7 +1,6 @@
 class OmniauthController < ApplicationController
     def github
-        p new_client.auth_code
-        access_token = new_client.auth_code.get_access_token(params[:code], :redirect_uri => "https://sample-devise-omniauth.netlify.app/#{access_token.token}")
+        access_token = new_client.auth_code.get_access_token(params[:code], :redirect_uri => "https://sample-devise-omniauth.netlify.app")
         p access_token.token
         redirect_to "https://sample-devise-omniauth.netlify.app/#{access_token.token}"
     end

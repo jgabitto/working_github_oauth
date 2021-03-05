6eb9b6660e63a22acda99f6cf755ab6e3957cb06
Jorge Gabitto <jgabitto1792@gmail.com>
2021-03-05 16:39:03 -0600

    added body to response

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -4,7 +4,7 @@ class OmniauthController < ApplicationController
     def github
         uri = URI('https://github.com/login/oauth/access_token')
         response = Net::HTTP.post_form(uri, 'client_id' => Rails.application.credentials.fetch(:client_id), 'client_secret' => Rails.application.credentials.fetch(:client_secret), 'code' => params[:code])
-        p response
+        p response.body
         redirect_to "https://sample-devise-omniauth.netlify.app/"
     end
 

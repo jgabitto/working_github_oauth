81f6db31c1af0116916bfd8371fe19ecbe3adb24
Jorge Gabitto <jgabitto1792@gmail.com>
2021-03-05 16:23:06 -0600

    removed a bunch of shit

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,16 +1,10 @@
 class OmniauthController < ApplicationController
     def github
-        access_token = new_client.auth_code.get_token(params[:code], :redirect_uri => "https://sample-devise-omniauth.netlify.app")
-        p access_token.token
-        redirect_to "https://sample-devise-omniauth.netlify.app/#{access_token.token}"
+        response = Net::HTTP.post_form('https://github.com/login/oauth/access_token', client_id: Rails.application.credentials.fetch(:client_id), client_secret: Rails.application.credentials.fetch((:client_secret), code: params[:code])
+        p response
+        redirect_to "https://sample-devise-omniauth.netlify.app/"
     end
 
-    private
-
-    def new_client
-        OAuth2::Client.new(Rails.application.credentials.fetch(:client_id), Rails.application.credentials.fetch(:client_secret), :site => 'https://github.com',
-          :authorize_path => '/login/oauth/authorize', :access_token_path => '/login/oauth/access_token')
-    end
 end
 
 # class Api::V1::OmniauthController < ApplicationController

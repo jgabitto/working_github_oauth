ebe30f15e631317aac4245fe8c4617272e967b03
Jorge Gabitto <jgabitto1792@gmail.com>
2021-03-05 16:34:26 -0600

    added ''

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -2,7 +2,7 @@ class OmniauthController < ApplicationController
     require 'net/http'
 
     def github
-        response = Net::HTTP.post_form('https://github.com/login/oauth/access_token', client_id => Rails.application.credentials.fetch(:client_id), client_secret => Rails.application.credentials.fetch(:client_secret), code => params[:code])
+        response = Net::HTTP.post_form('https://github.com/login/oauth/access_token', 'client_id' => Rails.application.credentials.fetch(:client_id), 'client_secret' => Rails.application.credentials.fetch(:client_secret), 'code' => params[:code])
         p response
         redirect_to "https://sample-devise-omniauth.netlify.app/"
     end

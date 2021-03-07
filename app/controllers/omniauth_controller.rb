5e6362de49b45e20ed414dae89b3386d94b7701e
Jorge Gabitto <jgabitto1792@gmail.com>
2021-03-07 11:53:52 -0600

    added variable

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -10,7 +10,8 @@ class OmniauthController < ApplicationController
         response = Net::HTTP.post_form(uri1, 'client_id' => Rails.application.credentials.fetch(:client_id), 'client_secret' => Rails.application.credentials.fetch(:client_secret), 'code' => params[:code])
         data = CGI::parse(response.body)
         p data
-        p data.values[0]
+        token = data.values[0]
+        p token
         # headers = {
         #     "Authorization" => "token #{data["access_token"]}",
         #     "User-Agent" => "jgabitto"
